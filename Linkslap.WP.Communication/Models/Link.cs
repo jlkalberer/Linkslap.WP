13e2e109647f348d54fe0809c76d716c7b227e66
John Kalberer <jlkalberer@gmail.com>
2014-06-19 15:26:31 -0700

    Changed property on link.

--- Linkslap.WP.Communication/Models/Link.cs
+++ Linkslap.WP.Communication/Models/Link.cs
@@ -2,29 +2,53 @@
 {
     using System;
 
+    using Newtonsoft.Json;
+
     /// <summary>
     /// The link.
     /// </summary>
     public class Link
     {
         /// <summary>
-        /// Gets or sets the id.
+        /// Gets or sets the feed name.
         /// </summary>
+        [JsonProperty("streamName")]
+        public string StreamName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the feed.
+        /// </summary>
+        [JsonProperty("streamKey")]
+        public string StreamKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the link id.
+        /// </summary>
+        [JsonProperty("id")]
         public int Id { get; set; }
 
         /// <summary>
         /// Gets or sets the url.
         /// </summary>
+        [JsonProperty("url")]
         public string Url { get; set; }
-
-        /// <summary>
-        /// Gets or sets the creation date.
-        /// </summary>
-        public DateTime CreatedDate { get; set; }
 
         /// <summary>
         /// Gets or sets the comment.
         /// </summary>
+        [JsonProperty("comment")]
         public string Comment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created date.
+        /// </summary>
+        [JsonProperty("createdDate")]
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user name.
+        /// </summary>
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
     }
 }
